b13036de04d70dd384d85aaf1ad01a65f65d77a3
sandarum <sandar.m7373@gmail.com>
2023-04-11 20:05:24 +0300

    Add function game

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+
+let computerScore = 0;
+let playerScore = 0;
+
 function getComputerChoice(){
 
     let choice;
@@ -27,10 +31,30 @@ function playRound(playerSelection, computerSelection){
         result = "It's a tie";
     }else if(playerSelectionLower === "rock" && computerSelection === "scissor" || playerSelectionLower === "paper" && computerSelection === "rock" || playerSelectionLower === "scissor" && computerSelection === "paper" ){
         result = `You win! ${playerSelectionLower} beats ${computerSelection}`
+        playerScore += 1;
     }else{
         result = `You lose! ${computerSelection} beats ${playerSelectionLower}`
+        computerScore += 1;
     }
     return result;
 }
 
+function game(){
+    for(let index = 0; index < 5; index++){
+        let playerSelection = prompt("Play...");
+        let computerSelection = getComputerChoice();
 
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    if(playerScore < computerScore){
+        console.log("Computer wins!!!");
+    }
+    else if(playerScore > computerScore){
+        console.log("You win!!!");
+    }else{
+        console.log("It's a tie");
+    }
+
+}
+
+game();
